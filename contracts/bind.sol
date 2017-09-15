2dfcd9285a30a48ec0056d5268228809115b3ba7
Leonardo Alt <leonardoaltt@gmail.com>
2017-09-16 00:03:31 +0200

    First version of smart contracts

--- contracts/bind.sol
+++ contracts/bind.sol
@@ -1,11 +1,20 @@
 pragma solidity ^0.4.11;
 
+import './contract.sol';
+
 contract Bind {
 
   address public owner;
- 
+  mapping (address => Contract[]) contracts1p; 
+  mapping (address => Contract[]) contracts2p; 
+
   modifier onlyOwner() {
     require(msg.sender == owner);
+    _;
+  }
+
+  modifier onlyAddress(address _addr) {
+    require(msg.sender == _addr);
     _;
   }
 
@@ -13,5 +22,27 @@ contract Bind {
     owner = msg.sender;
   }
 
+  function createContract(address _p2,
+                          Contract.PAY_TYPE _payType, 
+                          uint _payDate1, 
+                          uint _payValue, 
+                          uint _payDeposit,
+                          uint _endDate,
+                          string _desc) {
+    Contract _contract = new Contract(msg.sender, _p2, _payType, _payDate1, 
+                                      _payValue, _payDeposit, 
+                                      _endDate, _desc);
+    contracts1p[msg.sender].push(_contract);
+    contracts2p[_p2].push(_contract);
+  }
+
+  function getContracts1p() constant returns(Contract[]) {
+    return contracts1p[msg.sender];
+  }
+    
+  function getContracts2p() constant returns(Contract[]) {
+    return contracts2p[msg.sender];
+  }
+ 
 }
 
--- contracts/contract.sol
+++ contracts/contract.sol
@@ -2,15 +2,41 @@ pragma solidity ^0.4.11;
 
 contract Contract {
 
+  enum PAY_TYPE {
+    SINGLE,
+    WEEK,
+    MONTH,
+    YEAR
+  }
+
   address public owner;
- 
+  address party1;
+  address party2;
+  PAY_TYPE payType;
+  uint payDate1;
+  uint payValue;
+  uint payDeposit;
+  uint endDate;
+  string desc;
+
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
 
-  function Contract() {
+  function Contract(address _p1, address _p2,
+                    PAY_TYPE _payType, uint _payDate1,
+                    uint _payValue, uint _payDeposit,
+                    uint _endDate, string _desc) {
     owner = msg.sender;
+    party1 = _p1;
+    party2 = _p2;
+    payType = _payType;
+    payDate1 = _payDate1;
+    payValue = _payValue;
+    payDeposit = _payDeposit;
+    endDate = _endDate;
+    desc = _desc;
   }
 
 }
