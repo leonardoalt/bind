51be11dbd117bb7c750fd1ab194fdb345875d442
Leonardo Alt <leonardoaltt@gmail.com>
2017-09-16 14:04:04 +0200

    UI improvements and start print

---
+++ front/src/components/Printer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PrintTemplate from 'react-print';
+import QRCodeWriter from './qrcodeC/src';
+
+class Printer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <PrintTemplate>
+        <div style={{marginLeft:-210}}>
+          <h1>Contract between {this.props.sellerName} and {this.props.buyerName}</h1>
+          <h3>Value: {this.props.amount.toString()}</h3>
+          <p>
+            {this.props.desc}
+          </p>
+        </div>
+      </PrintTemplate>
+    )
+  }
+}
+export default Printer;
